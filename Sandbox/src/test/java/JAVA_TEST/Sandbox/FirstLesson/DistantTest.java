ea3def736b9ba7088306034718e0cfbe888e3e6a
Sergey Avdeenkov <32090146+itamitut@users.noreply.github.com>
2018-04-02 21:04:21 +0300

    Тесты для класса Point

--- Sandbox/src/test/java/JAVA_TEST/Sandbox/FirstLesson/DistantTest.java
+++ Sandbox/src/test/java/JAVA_TEST/Sandbox/FirstLesson/DistantTest.java
@@ -13,4 +13,20 @@ public class DistantTest {
     Point B = new Point(1.0,-2.0);
     Assert.assertEquals(4.0 , A.distance(B));
     }
+@Test
+    public void testDistance2(){
+        Point A = new Point(2.7,5.3);
+        Point B = new Point(1.2,-6.4);
+        Assert.assertEquals(B.distance(A) , A.distance(B));
+    }
+// Проверим Теорему Пифагора:
+@Test
+    public void testPyphagoras(){
+    Point A = new Point(-1.0,-1.0);
+    Point B = new Point(-1.0,2.0);
+    Point C = new Point(3.0,-1.0);
+    Assert.assertEquals( Math.pow(A.distance(B),2.0)
+            + Math.pow(A.distance(C),2.0), Math.pow(B.distance(C),2.0));
+
+    }
 }
