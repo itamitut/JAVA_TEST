39cd062ddffdf61b2dfb44b3cb91c54e2cdb55a3
Sergey Avdeenkov <32090146+itamitut@users.noreply.github.com>
2018-04-04 12:19:34 +0300

    04.04

---
+++ Lesson02/src/main/java/Lesson2.java
@@ -0,0 +1,10 @@
+
+/**
+ * Created by Сергей on 04.04.2018.
+ */
+public class Lesson2 {
+    public static void main(String[] args){
+        System.out.println("Ко второму уроку готов!");
+    }
+
+}
