d819817cdcee4c1c7d93f5ac25b0905679c7804f
Sergey Avdeenkov <32090146+itamitut@users.noreply.github.com>
2018-05-26 19:01:24 +0300

    6.9

--- addressbook/src/test/java/ru/addressbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/addressbook/tests/TestBase.java
@@ -1,13 +1,9 @@
 package ru.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.addressbook.appmanager.ApplicationManager;
-
-import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 
 /**
  * Created by Сергей on 09.04.2018.
